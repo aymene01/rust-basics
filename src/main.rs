e4333d5216c1a4b7b9742d15140d2c089ff72da9
Aymene Bousbia <aymene01@hotmail.fr>
2023-02-16 09:46:57 +0100

    controle flow + Result

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,42 @@ fn sum (s: &str) -> usize {
     s.split("+").map(|x| x.trim().parse::<usize>().unwrap_or(0)).sum()
 }
 
+fn get_number(s: &str) -> Result<u32, String> {
+ return match  s.parse::<u32>() {
+       Ok(n) => Ok(n),
+       Err(_) => Err("Not a number".to_string()),
+   };
+
+}
+
 fn main() {
     test_eq("1 + 1 = 2");
-    test_eq("3 + 2 = 4 + 1")
+    test_eq("3 + 2 = 4 + 1");
+    let array = [1, 2, 3];
+
+    let hello = 123;
+
+    if hello == 123 {
+        println!("Hello");
+    }
+
+    let increment_array = array.map(|x| {
+        match x {
+            1 => 9,
+            2 => 5,
+            3 => 0,
+            _ => 0,
+            
+        }
+    });
+
+    println!("{:?}", increment_array);
+
+    let result = get_number("1");
+    match result {
+        Ok(n) => println!("Number: {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
 }
+
+
